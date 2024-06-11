edf6b0d4da10d44734a801b67a60c7ed78324b22
Heidy <heidyguevara23@hotmail.com>
2024-06-11 10:12:12 +0200

    Add project files.

---
+++ CustomException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    internal class CustomException : Exception
+    {
+        public CustomException(string message) : base(message) 
+        {
+            ExceptionTime = DateTime.Now;
+        }
+        public DateTime ExceptionTime { get; private set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exceptions
+{
+    internal class Program
+    {
+        public static void ThrowError()
+        {
+            throw new CustomException("Houston, we have a problem !");
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                ThrowError();
+            }
+            catch (CustomException e) 
+            {
+                Console.WriteLine($"Message: {e.Message}");
+                Console.WriteLine($"Date: {e.ExceptionTime}");
+            }
+
+        }
+    }
+}
